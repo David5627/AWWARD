9ca993a8a5f08ccbd4729e66d55dd4944de95a99
David Kilonzo <binginaya708@gmail.com>
2021-01-25 11:29:20 +0300

    add profile function in views .py

--- users/views.py
+++ users/views.py
@@ -19,4 +19,22 @@ def register(request):
     
     return render(request, 'users/register.html', {'form': form})
 
-
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request,f'Your account has been updated successfully!')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        # import pdb; pdb.set_trace()
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+    return render(request, 'users/profile.html', context)
