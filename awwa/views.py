cbf81e47b11375f93f2ee2b7b740a964d217858b
David Kilonzo <binginaya708@gmail.com>
2021-01-25 11:01:25 +0300

    created index function in views.py

---
+++ awwa/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+from users.models import Profile
+import random
+
+
+def index(request):
+
+    try: 
+        posts= Post.objects.all()
+        posts = posts[::-1]
+        one_post = random.randint(0, len(posts)-1)
+        random_post= posts[one_post]
+        print(random_post)
+    except Post.DoesNotExist:
+        posts = None
+
+    return render(request, 'index.html', locals())
+
+
+
